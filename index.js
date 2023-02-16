9efcd4c8da91de954bfc5fa6de289d98a6bf0762
Konstantin <karpusharu@gmail.com>
2023-02-17 00:09:53 +0300

    сделать первую работу

--- index.js
+++ index.js
@@ -4,5 +4,12 @@ const personalMovieDB = {
     movies:{},
     actors:{},
     genres:[],
-    privat:false
+    private:false
 }
+const lastFilm1 = prompt('Один из последних просмотренных фильмов?');
+const ratingFilm1 = prompt('На сколько оцените его?');
+const lastFilm2 = prompt('Один из последних просмотренных фильмов?');
+const ratingFilm2 = prompt('На сколько оцените его?');
+personalMovieDB.movies[lastFilm1] = ratingFilm1;
+personalMovieDB.movies[lastFilm2] = ratingFilm2;
+console.log(personalMovieDB);
